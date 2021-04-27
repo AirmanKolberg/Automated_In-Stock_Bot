3a27c257726c96edd67201fb434723006cbb020b
T.J. Kolberg <71570804+AirmanKolberg@users.noreply.github.com>
2021-04-27 14:15:45 -0600

    🤫 Secret Files (requires configuration)

---
+++ secrets.py
@@ -0,0 +1,11 @@
+# This list should be filled with links to places where the product you desire is normally in-stock
+product_sites = ['https://www.isitinyet.com/my-product/123', 'http://www.howaboutnow.net/products/123/']
+
+# This list should contain a list of tags that are normally shown on your site(s) when out-of-stock
+out_of_stock_tags = ['out of stock', 'None in Stock', 'Out of Stock']
+
+# These will be custom to your Twilio account information and phone number
+twilio_sid = '6hjk356gkcg5k37vg75'
+twilio_token = 'b46hj64b3hjl643bhjl63b63l6'
+twilio_number = '+18668675309'
+my_number = '+1234567890'
